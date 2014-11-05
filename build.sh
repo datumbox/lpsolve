99fe44e1dc4242515fb014999fc165897d3b84fd
Vasilis Vryniotis <bbriniotis@datumbox.com>
2014-11-05 20:31:19 +0000

    update gitignore.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn clean package gpg:sign
-cd target
-jar -cvf bundle.jar lpsolve-*.pom lpsolve-*.pom.asc lpsolve-*.jar lpsolve-*.jar.asc lpsolve-*-javadoc.jar lpsolve-*-javadoc.jar.asc lpsolve-*-sources.jar lpsolve-*-sources.jar.asc
